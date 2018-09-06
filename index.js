23a9cdb45685b85b952be4b0aabf35d994d650a8
Bex Warner <bexmwarner@gmail.com>
2018-09-05 22:13:38 -0400

    add tests

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ module.exports = app => {
   app.on('pull_request.opened', async context => {
     const filesChanged = await context.github.pullRequests.getFiles(context.issue())
     const results = filesChanged.data.filter(file => file.filename.includes('.md'))
-    if (results) {
+    if (results && results.length > 0) {
       // make URLs
       let urls = ''
       await results.forEach(async (result) => {
-        console.log(result)
         urls += `\n[View rendered ${result.filename}](${context.payload.pull_request.head.repo.html_url}/blob/${context.payload.pull_request.head.ref}/${result.filename})`
       })
-      await context.github.pullRequests.update(context.issue({body: `${context.payload.pull_request.body} \n\n-----${urls}`}))
+      await context.github.pullRequests.update(context.issue({body: `${context.payload.pull_request.body}\n\n-----${urls}`}))
     }
   })
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,42 +1,52 @@
-const { Application } = require('probot')
-// Requiring our app implementation
-const myProbotApp = require('..')
+const nock = require('nock')
+const markdownify = require('..')
+const { Probot } = require('probot')
+const payload = require('./fixtures/pull_request.opened.json')
+const prFiles = require('./fixtures/prFiles.json')
+const payloadFail = require('./fixtures/payloadFail.json')
+const prFilesFail = require('./fixtures/prFilesFail.json')
 
-const issuesOpenedPayload = require('./fixtures/issues.opened.json')
+nock.disableNetConnect()
 
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
-})
-
-describe('My Probot app', () => {
-  let app, github
+describe('markdownify', () => {
+  let probot
 
   beforeEach(() => {
-    app = new Application()
-    // Initialize the app based on the code from index.js
-    app.load(myProbotApp)
-    // This is an easy way to mock out the GitHub API
-    github = {
-      issues: {
-        createComment: jest.fn().mockReturnValue(Promise.resolve({}))
-      }
-    }
-    // Passes the mocked out GitHub API into out app instance
-    app.auth = () => Promise.resolve(github)
+    probot = new Probot({})
+    const app = probot.load(markdownify)
+
+    // just return a test token
+    app.app = () => 'test'
   })
 
-  test('creates a comment when an issue is opened', async () => {
-    // Simulates delivery of an issues.opened webhook
-    await app.receive({
-      name: 'issues.opened',
-      payload: issuesOpenedPayload
-    })
+  test('edits the pull request when markdown files are editted', async () => {
+    nock('https://api.github.com')
+      .post('/app/installations/321696/access_tokens')
+      .reply(200, {token: 'test'})
 
-    // This test passes if the code in your index.js file calls `context.github.issues.createComment`
-    expect(github.issues.createComment).toHaveBeenCalled()
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/pulls/112/files')
+      .reply(200, prFiles.data)
+
+    nock('https://api.github.com')
+      .patch('/repos/hiimbex/testing-things/pulls/112', (body) => {
+        expect(body).toMatchObject({body: '\n\n-----\n[View rendered README.md](https://github.com/hiimbex/testing-things/blob/hiimbex-patch-41/README.md)'})
+        return true
+      })
+      .reply(200)
+
+    await probot.receive({name: 'pull_request', payload})
+  })
+
+  test('does not edit the pull request when no markdown files are editted', async () => {
+    nock('https://api.github.com')
+      .post('/app/installations/321696/access_tokens')
+      .reply(200, {token: 'test'})
+
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/pulls/114/files')
+      .reply(200, prFilesFail.data)
+
+    await probot.receive({name: 'pull_request', payload: payloadFail})
   })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
